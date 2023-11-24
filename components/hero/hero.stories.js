2d9bcfacc49545a352de19a5f506c2e7e8dc5774
James Mejia <james.mejia@bixal.com>
2023-11-24 16:13:57 -0600

    Add initial hero and generic section

---
+++ components/hero/hero.stories.js
@@ -0,0 +1,9 @@
+import Hero from "./hero.html.twig";
+
+export default {
+  title: "Components/Hero",
+  tags: ["autodocs"],
+  component: Hero,
+};
+
+export const Default = {};
---
+++ components/section/section.stories.js
@@ -0,0 +1,9 @@
+import Section from "./section.html.twig";
+
+export default {
+  title: "Components/Section",
+  tags: ["autodocs"],
+  component: Section,
+};
+
+export const Default = {};
