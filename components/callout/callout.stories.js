b331f97a8e2edd60769412c723cc6a8d6758a118
James Mejia <james.mejia@bixal.com>
2023-11-30 10:45:34 -0600

    Callout add basic styles

--- components/callout/callout.stories.js
+++ components/callout/callout.stories.js
@@ -1,6 +1,7 @@
 import Callout from "./callout.html.twig";
 import Content from "./callout.content.json";
 import "./callout.scss";
+import "../button/button.html.twig";
 
 export default {
   title: "Components/Callout",
