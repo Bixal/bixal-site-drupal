d9941bdf4e65b94f3a2a962c5b584000b1705745
James Mejia <james.mejia@bixal.com>
2023-12-01 14:34:16 -0600

    Add basic content styles

--- components/contact-us/contact-us.stories.js
+++ components/contact-us/contact-us.stories.js
@@ -1,7 +1,9 @@
 import ContactUs from "./contact-us.html.twig";
 
+import "./contact-us.scss";
 import "../section/section.scss";
 import "../button/button.html.twig";
+import "../button/button.scss";
 
 export default {
   title: "Components/Contact Us",
