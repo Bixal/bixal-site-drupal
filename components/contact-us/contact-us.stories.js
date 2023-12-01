ee350a6a29106c42dbdabe4422dd2f56986347fe
James Mejia <james.mejia@bixal.com>
2023-12-01 11:01:10 -0600

    Add basic footer styles

--- components/contact-us/contact-us.stories.js
+++ components/contact-us/contact-us.stories.js
@@ -1,5 +1,8 @@
 import ContactUs from "./contact-us.html.twig";
 
+import "../section/section.scss";
+import "../button/button.html.twig";
+
 export default {
   title: "Components/Contact Us",
   component: ContactUs,
--- components/footer/footer.stories.js
+++ components/footer/footer.stories.js
@@ -1,4 +1,11 @@
 import Footer from "./footer.html.twig";
+import "../social-nav/social-nav.html.twig";
+import "../button/button.html.twig";
+
+import "./footer.scss";
+import "../social-nav/social-nav.scss";
+import "../button/button.scss";
+import "../section/section.scss";
 
 export default {
   title: "Components/Footer",
