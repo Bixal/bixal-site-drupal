5aa397cf361a56010afd0786d5b33384dc4f9cdd
James Mejia <james.mejia@bixal.com>
2023-12-03 19:55:35 -0600

    People list add basic styles

--- components/people-list/people-list.stories.js
+++ components/people-list/people-list.stories.js
@@ -1,6 +1,8 @@
 import PeopleList from "./people-list.html.twig";
 import Content from "./people-list.content.json";
 
+import "./people-list.scss";
+
 export default {
   title: "Components/People list",
   tags: ["autodocs"],
