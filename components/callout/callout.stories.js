7ef94da4ec28a826e00b6c1f7ad997f1e9262f10
James Mejia <james.mejia@bixal.com>
2023-12-03 18:58:13 -0600

    Add callout centered variant

--- components/callout/callout.stories.js
+++ components/callout/callout.stories.js
@@ -10,4 +10,20 @@ export default {
   args: Content,
 };
 
-export const Default = {};
+export const Inverse = {
+  args: {
+    variant: "inverse",
+  },
+};
+
+export const Centered = {
+  args: {
+    variant: "centered",
+    image:
+      "https://www.bixal.com/static/img-careers-269d29ea5a43482c6c0b920bdd5e9d87.jpg",
+    prefix: "Work with us",
+    title:
+      "Bixal is filled with incredibly smart, creative, and passionate people. If you’re interested in joining our team, drop us a line or check out our careers page.",
+    description: null,
+  },
+};
