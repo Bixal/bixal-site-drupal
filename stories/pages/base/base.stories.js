e6aaf5ee2ec8b9ba3cdb203378bf355117d80c4e
James Mejia <james.mejia@bixal.com>
2023-12-07 18:44:01 -0600

    Refine hero and templates
    - Adjust type on hero
    - Use container for Blog landing

--- stories/pages/base/base.stories.js
+++ stories/pages/base/base.stories.js
@@ -1,9 +1,21 @@
 import Base from "./base.html.twig";
 
+import "../../components/header/header.stories";
+import "../../components/hero/hero.stories";
+import "../../components/contact-us/contact-us.stories";
+import "../../components/footer/footer.stories";
+
 export default {
   title: "Pages/Base",
   tags: ["autodocs"],
   component: Base,
 };
 
-export const Default = {};
+export const Default = {
+  args: {
+    hero: {
+      variant: "image-bg",
+      title: "Hero title",
+    },
+  },
+};
--- stories/pages/blog-post/blog-post.stories.js
+++ stories/pages/blog-post/blog-post.stories.js
@@ -6,4 +6,13 @@ export default {
   component: BlogPost,
 };
 
-export const All = {};
+export const All = {
+  args: {
+    hero: {
+      variant: "image-bg",
+      title: "Blog and News",
+      image:
+        "https://www.bixal.com/static/0af8355f1979d79ddb8e2b9ab90b3a49/blog-and-news-header.png",
+    },
+  },
+};
