cb4e9d137c23a4079e5eca538f1b86f254448989
James Mejia <james.mejia@bixal.com>
2023-12-07 18:43:13 -0600

    Refine blurbs

--- stories/components/blurb/blurb.stories.js
+++ stories/components/blurb/blurb.stories.js
@@ -1,8 +1,22 @@
 import Blurb from "./blurb.html.twig";
+import BlurbCollectionTemplate from "./blurb-collection.html.twig";
+import BlurbCollectionContent from "./blurb-collection.content.json";
+
+import "./blurb.scss";
 
 export default {
   title: "Components/Blurb",
   component: Blurb,
+  args: {
+    title: "Key Learnings for a More Inclusive Global Health Landscape",
+    description: null,
+    date: "November 30, 2023",
+  },
 };
 
 export const Default = {};
+
+export const BlurbCollection = {
+  args: BlurbCollectionContent,
+  render: BlurbCollectionTemplate,
+};
