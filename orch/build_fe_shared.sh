e6685a619bab4643d59f2f3edd70d00361c6af23
Buddy Harlow <buddy.harlow@bixal.com>
2023-12-13 12:33:31 -0500

    BSD-12: build tasks to pull in storybook assets

--- orch/build_fe_shared.sh
+++ orch/build_fe_shared.sh
@@ -5,9 +5,48 @@ set -e
 
 green='\033[0;32m'
 NC='\033[0m'
-echo "Front end can be built by using the file ./orc/build_fe_shared.sh, it is Node 18"
+echo "Front end can be built by using the file ./orch/build_fe_shared.sh, it is Node 18"
 
+echo "Updating storybook components in theme"
+source_dir="./components"
+components_target_dir="./web/themes/custom/bixal_uswds/templates/components/storybook_components"
+sass_target_dir="./web/themes/custom/bixal_uswds/src/sass/storybook_sass"
+js_target_dir="./web/themes/custom/bixal_uswds/src/js/storybook_js"
 
+if [ -d $source_dir ] 
+then
+    
+    if [ -d $components_target_dir ]
+    then
+        echo -e "${green}Removing theme components folder${NC}"
+        rm -R $components_target_dir
+    else 
+        echo ""
+    fi
+    if [ -d $sass_target_dir ]
+    then
+        echo -e "${green}Removing theme sass folder${NC}"
+        rm -R $sass_target_dir
+    else 
+        echo ""
+    fi
+    if [ -d $js_target_dir ]
+    then
+        echo -e "${green}Removing theme js folder${NC}"
+        rm -R $js_target_dir
+    else 
+        echo ""
+    fi
+
+    echo -e "${green}Copying theme components folders${NC}"
+    mkdir $components_target_dir $sass_target_dir $js_target_dir
+    find "$source_dir" -name '*.twig' -exec cp {} "$components_target_dir" \;
+    find "$source_dir" -name '*.scss' -exec cp {} "$sass_target_dir" \;
+    find "$source_dir" -name '*.js' -exec cp {} "$js_target_dir" \;
+    find "$js_target_dir" -name '*.stories.js' -delete
+else
+    echo ""
+fi
 
 cd web/themes/custom/bixal_uswds
 
