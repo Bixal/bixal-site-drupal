21f6c88cd9c9845f9910298f2c2090179989028c
Matt Poole <mpbixal@gmail.com>
2024-01-23 15:49:17 -0500

    BSD fixes #48: Re-use the social-nav story so that only the story needs to be imported, not the data and the css.

--- stories/components/footer/footer.stories.js
+++ stories/components/footer/footer.stories.js
@@ -2,17 +2,16 @@ import Footer from "./footer.html.twig";
 import "../button/button.html.twig";
 
 import "./footer.scss";
-import "../social-nav/social-nav.scss";
 import "../button/button.scss";
 import "../section/section.scss";
 
-import SocialNavContent from "../social-nav/social-nav.content.json";
+import * as SocialNav from "../social-nav/social-nav.stories";
 
 export default {
   title: "Components/Footer",
   component: Footer,
   args: {
-    socialNav: SocialNavContent
+    socialNav: SocialNav.default.args
   }
 };
 
