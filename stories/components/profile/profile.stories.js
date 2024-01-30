1334392b1231191e29b7e8003757f440d6c11c9c
James Mejia <james.mejia@bixal.com>
2024-01-30 11:44:08 -0600

    BSD closes #52: Create profile page.

--- stories/components/profile/profile.stories.js
+++ stories/components/profile/profile.stories.js
@@ -1,7 +1,7 @@
 import Profile from "./profile.html.twig";
 
-import "../social-nav/social-nav.content.json";
-import "../social-nav/social-nav.stories";
+import ProfileContent from "./profile.content.json";
+import * as SocialNav from "../social-nav/social-nav.stories";
 
 import "./profile.scss";
 
@@ -9,15 +9,8 @@ export default {
   title: "Components/Profile",
   component: Profile,
   args: {
-    socialNav: {
-      variant: "accent-warm",
-      items: [
-        {
-          "href": "https://www.linkedIn.com",
-          "label": "LinkedIn"
-        }
-      ]
-    }
+    socialNav: SocialNav.default,
+    ...ProfileContent
   }
 };
 
