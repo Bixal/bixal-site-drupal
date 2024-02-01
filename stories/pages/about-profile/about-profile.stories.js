d6f43a44202bdf1c6e54d102265541ada38e28ff
James Mejia <james.mejia@bixal.com>
2024-01-31 21:52:51 -0600

    BSD fixes #52: Add missing components to Profile page.

--- stories/pages/about-profile/about-profile.stories.js
+++ stories/pages/about-profile/about-profile.stories.js
@@ -3,6 +3,8 @@ import AboutProfile from "./about-profile.html.twig";
 import * as Header from "../../components/header/header.stories";
 import * as Profile from "../../components/profile/profile.stories";
 import * as SocialNav from "../../components/social-nav/social-nav.stories";
+import "../../components/contact-us/contact-us.stories";
+import "../../components/footer/footer.stories";
 
 export default {
   title: "Pages/About/Profile",
