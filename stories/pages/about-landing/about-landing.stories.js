fc2a85e8122c5ed7929edefed0900b495d0cd077
James Mejia <james.mejia@bixal.com>
2024-02-06 19:06:12 -0600

    BSD closes #73: About landing page; remove tall variant.

--- stories/pages/about-landing/about-landing.stories.js
+++ stories/pages/about-landing/about-landing.stories.js
@@ -25,7 +25,6 @@ export const Default = {
       description: "<p>We do this by partnering with leading federal agencies to conceive and create powerful data-driven customer experiences to better serve the American public and communities around the world.</p><p>Bixal is more than an organization. Bixal is a methodology. It’s a people-absolutely-first approach to solving complex organizational challenges, balancing cutting-edge technical chops with a deep sense of empathy and understanding.</p>",
     },
     bixalers: {
-      variant: "tall",
       image: "https://www.bixal.com/static/59f4525c9919bfa346d4f9cf50f1070a/5e5f3/about--b-banner.webp",
     },
     people: PeopleList.default.args,
