875236bb5548f602493151625191ede91ea7844e
James Mejia <james.mejia@bixal.com>
2024-02-15 12:55:52 -0600

    BSD closes #51: Create initial Contract Vehicle pages in StorybookJS.

---
+++ stories/components/contract-vehicle/contract-vehicle.stories.js
@@ -0,0 +1,17 @@
+import ContractVehicle from "./contract-vehicle.html.twig"
+
+export default {
+  title: "Components/Contract Vehicle",
+  tags: ["autodocs"],
+  component: ContractVehicle,
+  args: {
+    overview: "<p>Bixal is the sole holder of a blanket purchase agreement (BPA) providing independent monitoring and evaluation (M&E) services in support of the U.S. Department of the Treasury’s Office of Technical Assistance (OTA). With over 90 technical assistance projects in 51 developing and transitional countries around the globe, the OTA works with finance ministries and central banks to safeguard financial sectors and strengthen their ability to manage public finances effectively. M&E services under this BPA include designing and conducting evaluations of OTA initiatives, supporting M&E process improvements and facilitating program learning.</p><p>This three-year BPA builds on Bixal’s M&E capabilities, which were significantly augmented in January 2021, when we acquired Clear Outcomes. As Bixal furthers our data science and analytics practice area, we also continue to enhance our M&E services by integrating them with our transformational technology and data-driven capabilities.</p>",
+    marketing: "",
+    capabilities: "",
+    contact: "",
+    documents: "",
+    contractInformation: {},
+  }
+};
+
+export const Default = {};
