86cf2eab75baec06e5378523412be9f2ad01990b
Buddy Harlow <buddy.harlow@bixal.com>
2024-05-02 12:36:45 -0400

    BSD closes #113: created case studies module, added all assets to static dir, expanded build_fe script.

--- stories/components/hero/hero.stories.js
+++ stories/components/hero/hero.stories.js
@@ -13,7 +13,7 @@ export const BackgroundImage = {
   args: {
     variant: "image-bg",
     image:
-      "https://www.bixal.com/static/0a66c21e57fc6b5c25b6a4b9055bf84f/landing-hero.jpg",
+      "static/0a66c21e57fc6b5c25b6a4b9055bf84f/landing-hero.jpg",
   },
 };
 
@@ -21,7 +21,7 @@ export const InlineImage = {
   args: {
     variant: "image-inline",
     image:
-      "https://www.bixal.com/static/5fe5ae2fa9f9bc458b9dd0914c0b1c9a/ce7bb/img-b-hero-7.webp",
+      "static/5fe5ae2fa9f9bc458b9dd0914c0b1c9a/ce7bb/img-b-hero-7.webp",
     title: "This is Bixal.",
     description:
       "A mission-driven organization determined to improve people’s lives through human-centered strategies and transformative technologies. We deliver on this promise by partnering with leading federal agencies to conceive and create powerful data-driven customer experiences.",
