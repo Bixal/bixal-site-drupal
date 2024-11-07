7b338d7044ab1debd3e6b19c2e418d06b179bb10
Matt Poole <mattsqd@users.noreply.github.com>
2024-11-07 17:12:38 -0500

    BSD fixes #338: Allow cache to be cleared with Drush again.

--- web/sites/default/settings.memcache.php
+++ web/sites/default/settings.memcache.php
@@ -8,35 +8,37 @@ if ((
   file_exists($app_root . '/modules/contrib/memcache') &&
   !function_exists('_settings_memcache')
 )) {
+  // These settings need to be applied multiple times, depending on how often
+  // initialize is called, they need to be reapplied every time.
   function _settings_memcache(array &$settings, string $host): void {
     $settings['memcache']['servers'][$host] = 'default';
+
+    # Use for all bins otherwise specified.
+    $settings['cache']['default'] = 'cache.backend.memcache';
+
+    // Optional settings:
+
+    // Apply changes to the container configuration to better leverage Memcache.
+    // This includes using Memcache for the lock and flood control systems, as well
+    // as the cache tag checksum. Alternatively, copy the contents of that file
+    // to your project-specific services.yml file, modify as appropriate, and
+    // remove this line.
+    $settings['container_yamls'][] = 'modules/contrib/memcache/example.services.yml';
+
+    // Allow the services to work before the Memcache module itself is enabled.
+    $settings['container_yamls'][] = 'modules/contrib/memcache/memcache.services.yml';
+
+    // Use Memcache for container cache.
+    // The container cache is used to load the container definition itself, and
+    // thus any configuration stored in the container itself is not available
+    // yet. These lines force the container cache to use Memcache rather than the
+    // default SQL cache.
+    require 'settings.memcache.container_pure.php';
   }
 
-  # Use for all bins otherwise specified.
-  $settings['cache']['default'] = 'cache.backend.memcache';
-
-  // Optional settings:
-
-  // Apply changes to the container configuration to better leverage Redis.
-  // This includes using Memcache for the lock and flood control systems, as well
-  // as the cache tag checksum. Alternatively, copy the contents of that file
-  // to your project-specific services.yml file, modify as appropriate, and
-  // remove this line.
-  $settings['container_yamls'][] = 'modules/contrib/memcache/example.services.yml';
-
-  // Allow the services to work before the Redis module itself is enabled.
-  $settings['container_yamls'][] = 'modules/contrib/memcache/memcache.services.yml';
+  // These only need to be done once, then they are included and applied always.
 
   // Manually add the classloader path, this is required for the container cache bin definition below
-  // and allows to use it without the redis module being enabled.
+  // and allows to use it without the Memcache module being enabled.
   $class_loader->addPsr4('Drupal\\memcache\\', 'modules/contrib/memcache/src');
-
-  require 'settings.memcache.container_pure.php';
 }
-
-
-
-
-
-
-
--- web/sites/default/settings.redis.php
+++ web/sites/default/settings.redis.php
@@ -8,35 +8,39 @@ if ((
   class_exists('Drupal\redis\ClientFactory') &&
   !function_exists('_settings_redis')
 )) {
+  // These settings need to be applied multiple times, depending on how often
+  // initialize is called, they need to be reapplied every time.
   function _settings_redis(array &$settings, string $host, string $port): void {
     $settings['redis.connection']['host'] = $host;
     $settings['redis.connection']['port'] = $port;
+    $settings['redis.connection']['interface'] = 'PhpRedis'; // Can be "Predis".
+    # Use for all bins otherwise specified.
+    $settings['cache']['default'] = 'cache.backend.redis';
+
+    // Optional settings:
+
+    // Apply changes to the container configuration to better leverage Redis.
+    // This includes using Redis for the lock and flood control systems, as well
+    // as the cache tag checksum. Alternatively, copy the contents of that file
+    // to your project-specific services.yml file, modify as appropriate, and
+    // remove this line.
+    $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+
+    // Allow the services to work before the Redis module itself is enabled.
+    $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+
+    // Use redis for container cache.
+    // The container cache is used to load the container definition itself, and
+    // thus any configuration stored in the container itself is not available
+    // yet. These lines force the container cache to use Redis rather than the
+    // default SQL cache.
+    require 'settings.redis.container.php';
   }
 
-  $settings['redis.connection']['interface'] = 'PhpRedis'; // Can be "Predis".
-  # Use for all bins otherwise specified.
-  $settings['cache']['default'] = 'cache.backend.redis';
-
-  // Optional settings:
-
-  // Apply changes to the container configuration to better leverage Redis.
-  // This includes using Redis for the lock and flood control systems, as well
-  // as the cache tag checksum. Alternatively, copy the contents of that file
-  // to your project-specific services.yml file, modify as appropriate, and
-  // remove this line.
-  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
-
-  // Allow the services to work before the Redis module itself is enabled.
-  $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+  // These only need to be done once, then they are included and applied always.
 
   // Manually add the classloader path, this is required for the container cache bin definition below
   // and allows to use it without the redis module being enabled.
   $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
 
-  require 'settings.redis.container.php';
 }
-
-
-
-
-
