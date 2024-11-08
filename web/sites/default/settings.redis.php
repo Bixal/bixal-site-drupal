c2c1eca222a7be81a0d3869342e66f0256859a70
Matt Poole <mattsqd@users.noreply.github.com>
2024-11-08 13:08:48 -0500

    BSD fixes #338: Make so that Platform could re-use the same settings function as Lando. Added support for memcached in platform.

--- web/sites/default/settings.redis.php
+++ web/sites/default/settings.redis.php
@@ -2,28 +2,44 @@
 
 use Drupal\Core\Installer\InstallerKernel;
 
-if ((
+if (
   !InstallerKernel::installationAttempted() &&
   extension_loaded('redis') &&
-  class_exists('Drupal\redis\ClientFactory') &&
-  !function_exists('_settings_redis')
-)) {
-  // These settings need to be applied multiple times, depending on how often
-  // initialize is called, they need to be reapplied every time.
-  function _settings_redis(array &$settings, string $host, string $port): void {
+  class_exists('Drupal\redis\ClientFactory')
+) {
+  /**
+   * Apply Redis cache settings.
+   *
+   * Drush can be bootstrap Drupal twice, this should be safe to be called
+   * multiple times.
+   *
+   * @param array $settings
+   *   The $settings array from settings.php.
+   * @param string $host
+   *   The host that Redis will be contacted on.
+   * @param string $port
+   *   The port that Redis will be contacted on.
+   * @param string $interface
+   *   This can be 'Relay', 'PhpRedis', or 'Predis'. PhpRedis is fastest, Predis
+   *
+   * @return void
+   */
+  function _drupal_env_settings_redis(array &$settings, string $host, string $port, string $interface = ''): void {
     $settings['redis.connection']['host'] = $host;
     $settings['redis.connection']['port'] = $port;
-    $settings['redis.connection']['interface'] = 'PhpRedis'; // Can be "Predis".
-    # Use for all bins otherwise specified.
+    if (!empty($interface)) {
+      $settings['redis.connection']['interface'] = $interface;
+    }
+    // Use for all bins otherwise specified.
     $settings['cache']['default'] = 'cache.backend.redis';
 
-    // Optional settings:
+    /* Optional settings:
 
-    // Apply changes to the container configuration to better leverage Redis.
-    // This includes using Redis for the lock and flood control systems, as well
-    // as the cache tag checksum. Alternatively, copy the contents of that file
-    // to your project-specific services.yml file, modify as appropriate, and
-    // remove this line.
+    Apply changes to the container configuration to better leverage Redis.
+    This includes using Redis for the lock and flood control systems, as well
+    as the cache tag checksum. Alternatively, copy the contents of that file
+    to your project-specific services.yml file, modify as appropriate, and
+    remove this line. */
     $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
 
     // Allow the services to work before the Redis module itself is enabled.
@@ -38,7 +54,6 @@ if ((
   }
 
   // These only need to be done once, then they are included and applied always.
-
   // Manually add the classloader path, this is required for the container cache bin definition below
   // and allows to use it without the redis module being enabled.
   $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
