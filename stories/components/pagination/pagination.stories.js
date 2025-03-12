f3f64340267619c314fbf93e5b29043a2c8abf3b
James Mejia <james.mejia@bixal.com>
2025-03-12 10:38:41 -0500

    BSD fixes #380: Add Drupal pager component.

---
+++ stories/components/pagination/pagination.stories.js
@@ -0,0 +1,106 @@
+import Pagination from "./pagination.html.twig";
+import "./pagination.scss";
+
+export default {
+  title: "Components/Pagination",
+  tags: ["autodocs"],
+  component: Pagination,
+};
+
+const data = {
+  headingId: "pagination-heading",
+  items: {
+    first: {
+      href: "https://example.com/page/1",
+      attributes: {
+        class: ["first", "page-item"],
+        "aria-label": "Go to first page",
+      },
+      text: "First",
+    },
+    pages: {
+      1: {
+        href: "https://example.com/page/1",
+        attributes: {
+          class: ["current", "page-item"],
+          "aria-current": "page",
+        },
+        text: "1",
+      },
+      2: {
+        href: "https://example.com/page/2",
+        attributes: {
+          class: ["page-item"],
+        },
+        text: "2",
+      },
+      3: {
+        href: "https://example.com/page/3",
+        attributes: {
+          class: ["page-item"],
+          "aria-label": "Go to page 3",
+        },
+        text: "3",
+      },
+      4: {
+        href: "https://example.com/page/4",
+        attributes: {
+          class: ["page-item"],
+          "aria-label": "Go to page 4",
+        },
+        text: "4",
+      },
+      5: {
+        href: "https://example.com/page/5",
+        attributes: {
+          class: ["page-item"],
+          "aria-label": "Go to page 5",
+        },
+        text: "5",
+      },
+      6: {
+        href: "https://example.com/page/6",
+        attributes: {
+          class: ["page-item"],
+          "aria-label": "Go to page 6",
+        },
+        text: "6",
+      },
+      7: {
+        href: "https://example.com/page/7",
+        attributes: {
+          class: ["page-item"],
+          "aria-label": "Go to page 7",
+        },
+        text: "7",
+      },
+    },
+    // next: {
+    //   href: "https://example.com/page/5",
+    //   attributes: {
+    //     class: ["next", "page-item"],
+    //     "aria-label": "Go to next page",
+    //   },
+    //   text: "Next",
+    // },
+    last: {
+      href: "https://example.com/page/7",
+      attributes: {
+        class: ["last", "page-item"],
+        "aria-label": "Go to last page",
+      },
+      text: "Last",
+    },
+  },
+  current: 1,
+  ellipses: {
+    previous: true,
+    next: true,
+  },
+};
+
+export const Default = {
+  args: {
+    ...data,
+  },
+};
