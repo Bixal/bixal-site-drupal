55a06220233bdda4ed6631b5c114b888f2fd7e31
James Mejia <james.mejia@bixal.com>
2025-04-02 10:53:19 -0500

    BSD fixes #124: Create initial prose component based on blog post.

---
+++ stories/components/prose/prose.stories.js
@@ -0,0 +1,11 @@
+import Prose from "./prose.html.twig";
+
+export default {
+  title: "Components/Typography/Prose",
+  argTypes: {
+    content: { control: "text" },
+  },
+  component: Prose,
+};
+
+export const Default = {};
