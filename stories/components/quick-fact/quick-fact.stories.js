f1ec03bc00a6792a2fa29bb86867bd79391cdf03
James Mejia <james.mejia@bixal.com>
2025-07-02 08:28:09 -0500

    BSD fixes #484: Create quick fact prototype.

---
+++ stories/components/quick-fact/quick-fact.stories.js
@@ -0,0 +1,59 @@
+// import infoIcon from "@uswds/uswds/img/usa-icons/info_outline.svg";
+import QuickFact from "./quick-fact.html.twig";
+
+const items = [
+  {
+    title: "60%",
+    description:
+      "Reduction in monthly infrastructure costs for SBA.gov—driven by cloud modernization.",
+  },
+  {
+    title: "200+",
+    description:
+      "Federal websites are powered by components built and designed by Bixal.",
+  },
+  {
+    title: "7.5<small>B</small>",
+    description:
+      "Megabytes of Medicaid & CHIP data unified—now used across 13 CMS offices and 7 agencies.",
+  },
+];
+
+export default {
+  title: "Components/Quick Fact",
+  tags: ["autodocs"],
+  component: QuickFact,
+  args: {
+    variant: null,
+    layout: null,
+    items: items.slice(0, 1),
+  },
+};
+
+export const Default = {};
+
+export const AccentCool = {
+  args: {
+    variant: "accent-cool",
+  },
+};
+
+export const Warm = {
+  args: {
+    variant: "accent-warm",
+  },
+};
+
+export const CollectionHalves = {
+  args: {
+    layout: "halves",
+    items: items.slice(0, 2),
+  },
+};
+
+export const CollectionThirds = {
+  args: {
+    layout: "thirds",
+    items: items,
+  },
+};
