72b3638d0d1b814d574350eafe3d3b9f9ba968c9
James Mejia <james.mejia@bixal.com>
2025-09-05 11:56:31 -0500

    BSD fixes #492: Update stylelint rules based on project preferences.
    Disabled the following:
    - `custom-property-pattern` because we're using private CSS vars to define component defaults
    - `selector-class-pattern` because we're using BEM syntax and I didn't want to add another dependency for it

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,8 @@
 /** @type { import('stylelint').Config } */
 export default {
   extends: ["stylelint-config-standard-scss", "stylelint-prettier/recommended"],
+  rules: {
+    "custom-property-pattern": null, // Allows us to set private CSS vars.
+    "selector-class-pattern": null, // Disabled because we use BEM.
+  },
 };
