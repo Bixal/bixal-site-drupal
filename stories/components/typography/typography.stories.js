11b85a4e19ceca4b7cc542e77e703583c8008d74
James Mejia <james.mejia@bixal.com>
2025-10-16 12:42:20 -0500

    BSD fixes #513: Create text util classes for brand colors.

--- stories/components/typography/typography.stories.js
+++ stories/components/typography/typography.stories.js
@@ -1,6 +1,8 @@
 import Typography from "./typography.html.twig";
 import "../description-list/description-list.html.twig";
 
+const lorem = `Lorem, ipsum dolor sit amet consectetur adipisicing elit.`
+
 export default {
   title: "Components/Typography",
   tags: ["autodocs"],
@@ -8,3 +10,13 @@ export default {
 };
 
 export const All = {};
+
+export const Colors = {
+  render: () => `
+    <div class="text-primary">${lorem}</div>
+    <div class="text-primary-alt">${lorem}</div>
+    <div class="text-accent-cool">${lorem}</div>
+    <div class="text-accent-warm">${lorem}</div>
+    <div class="text-accent-vivid">${lorem}</div>
+  `,
+}
