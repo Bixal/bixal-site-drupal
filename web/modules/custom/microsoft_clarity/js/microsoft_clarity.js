976675b2dff77c8d391a12a408ae67a29f385281
James Mejia <james.mejia@bixal.com>
2026-09-04 10:09:20 -0500

    BSD fixes #611: Format MS Clarity JS

--- web/modules/custom/microsoft_clarity/js/microsoft_clarity.js
+++ web/modules/custom/microsoft_clarity/js/microsoft_clarity.js
@@ -7,7 +7,7 @@
  * js/cookies_microsoft_clarity.js.
  */
 (function (drupalSettings) {
-  'use strict';
+  "use strict";
 
   var settings = drupalSettings.microsoft_clarity || {};
 
@@ -16,15 +16,22 @@
   }
 
   (function (c, l, a, r, i, t, y) {
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); };
-    t = l.createElement(r); t.async = 1; t.src = 'https://www.clarity.ms/tag/' + i;
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-  })(window, document, 'clarity', 'script', settings.projectId);
+    c[a] =
+      c[a] ||
+      function () {
+        (c[a].q = c[a].q || []).push(arguments);
+      };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+  })(window, document, "clarity", "script", settings.projectId);
 
   // The snippet above defines clarity() as a queue, so tags set here are
   // replayed once the remote tag has loaded.
   var tags = settings.tags || {};
   Object.keys(tags).forEach(function (name) {
-    window.clarity('set', name, tags[name]);
+    window.clarity("set", name, tags[name]);
   });
 })(drupalSettings);
